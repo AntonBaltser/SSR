8921e87663f8360e64cf8221d2443143a4344c20
AntonBaltser <antonbaltser@yahoo.com>
2022-01-31 00:47:28 +0300

    s3

--- src/minio/connection.js
+++ src/minio/connection.js
@@ -1,9 +1,11 @@
 const AWS = require("aws-sdk");
 
 module.exports  = new AWS.S3({
-    accessKeyId: 'antony' ,
-    secretAccessKey: 'bp024Wbp024W' ,
-    endpoint: 'http://192.168.1.72:9000' ,
-    s3ForcePathStyle: true, // needed with minio?
-    signatureVersion: 'v4'
+    accessKeyId: process.env.S3_KEY,
+    secretAccessKey: process.env.S3_SECRET
+    // accessKeyId: 'antony' ,
+    // secretAccessKey: 'bp024Wbp024W' ,
+    // endpoint: 'http://192.168.1.72:9000' ,
+    // s3ForcePathStyle: true, // needed with minio?
+    // signatureVersion: 'v4'
 });
